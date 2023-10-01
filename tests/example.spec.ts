09cad99693ef38b8da8503c14265e62e4faa2e84
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-10-01 12:26:35 +0200

    test executed with terminal

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.only('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -16,3 +16,12 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+/**
+ * npx playwright test
+ * npx playwright show-report
+ * npx playwright test --project=chromium --headed
+ * npx playwright test example.spec.ts  --project=chromium
+ * npx playwright test -g "has title" --project=chromium ->only one test for execut 
+ * 
+ */
